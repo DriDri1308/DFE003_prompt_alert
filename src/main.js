d1c0d65db7f267e991e9736fc8c5b6038a3d63c3
DriDri1308 <dada2011livia23@gmail.com>
2023-10-24 20:30:46 -0300

    parte final exercicio

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
+//exercicio 1
+
 const numero = prompt ("olá, insira um numero para te dar o dobro");
 const dobro = numero * 2;
 
 alert(" O dobro do numero informado é: " + dobro)
+
+//exercicio 2
+
+const totalAPagar = prompt("Digite o valor da conta do jantar: ");
+const valor = totalAPaga * 0.1;
+const somaTotal = Number(totalAPaga) + Number(valor);
+alert(`O valor total da conta é ${somaTotal}`);
+
+//exercicio 3 
+
+const pizzeriaDri = prompt("Digite o valor da conta da pizzaria: ");
+const clientes = prompt("Digite a quantidade de clientes: ");
+const totalClientes = pizzeriaPrice / clientes;
+alert(`O valor a ser pago por cliente é {totalClientes}`);
+
